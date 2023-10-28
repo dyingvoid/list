2bf8b6a335fef954b4672f3fe2e1fcd389b6b6b6
Dying <dyingvoid@gmail.com>
2023-10-28 14:26:58 +0500

    Add project files.

---
+++ list/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new LinkedList<int>();
+            for(int i = 0; i < 10; i++)
+            {
+                list.Insert(i);
+            }
+
+            list.Print();
+            list.Reverse();
+            list.Print();
+        }
+    }
+
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T>? Next { get; set; }
+        public Node<T>? Prev { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+            Prev = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        public Node<T> Head { get; set; }
+
+        public void Insert(T data)
+        {
+            var node = new Node<T>(data);
+            node.Next = Head;
+            
+            if(Head != null)
+                Head.Prev = node;
+
+            Head = node;
+        }
+
+        public Node<T> Last()
+        {
+            var temp = Head;
+
+            while(temp.Next != null)
+                temp = temp.Next;
+
+            return temp;
+        }
+
+        public void Reverse()
+        {
+            Node<T> next = null;
+            Node<T> prev = null;
+            Node<T> current = Head;
+
+            while(current != null)
+            {
+                next = current.Next;
+                current.Next = current.Prev;
+                prev = current;
+                current = next;
+            }
+
+            Head = prev;
+        }
+
+        public void Print()
+        {
+            var temp = Head;
+            Console.Write(temp.Data + " -> ");
+
+            while(temp.Next != null)
+            {
+                Console.Write(temp.Next.Data + " -> ");
+                temp = temp.Next;
+            }
+
+            Console.WriteLine("NULL");
+        }
+    }
+}
